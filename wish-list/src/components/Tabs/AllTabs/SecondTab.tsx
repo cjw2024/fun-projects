7cf06a75ae149ad69c28cb3661f9c5e00e0c1a55
Caden Wright <cjw224@lehigh.edu>
2023-07-22 18:32:27 -0400

    filled entire screen with tabs menu

--- wish-list/src/components/Tabs/AllTabs/SecondTab.tsx
+++ wish-list/src/components/Tabs/AllTabs/SecondTab.tsx
@@ -2,12 +2,32 @@
 
 import React from "react";
 import './AllTabs.css';
+import Card from "../../Cards/Card";
+import { BeakerIcon } from "@heroicons/react/24/solid";
 
 const SecondTab = () => {
   return (
     <div className="SecondTab">
       <p>Christmas List</p>
       {/* Second  tab content will go here */}
+      <Card
+        body="body"
+        title="title" 
+        indicator="indicator"
+        subtitle="subtitle"
+        image="http://source.unsplash.com/random"
+        badge={{
+          text: "Badge",
+          filled: false
+        }}
+        btn={{
+          text: "Link",
+          type: "primary",
+          href: "#",
+          filled: true,
+          icon: <BeakerIcon />
+        }}
+      />
     </div>
   );
 };
--- wish-list/src/components/Tabs/AllTabs/ThirdTab.tsx
+++ wish-list/src/components/Tabs/AllTabs/ThirdTab.tsx
@@ -2,12 +2,32 @@
 
 import React from "react";
 import './AllTabs.css';
+import Card from "../../Cards/Card";
+import { BeakerIcon } from "@heroicons/react/24/solid";
 
 const ThirdTab = () => {
   return (
     <div className="ThirdTab">
       <p>Graduation List</p>
       {/* third  tab content will go here */}
+      <Card
+        body="body"
+        title="title" 
+        indicator="indicator"
+        subtitle="subtitle"
+        image="http://source.unsplash.com/random"
+        badge={{
+          text: "Badge",
+          filled: false
+        }}
+        btn={{
+          text: "Link",
+          type: "primary",
+          href: "#",
+          filled: true,
+          icon: <BeakerIcon />
+        }}
+      />
     </div>
   );
 };
